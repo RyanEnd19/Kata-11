4d3c08093fa144d8bde6bb66f54f3a85c1259f97
RyanEnd19 <olisov_vbk@bk.ru>
2023-04-26 20:18:16 +0300

    Задача выполнена

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -3,10 +3,7 @@ package jm.task.core.jdbc.dao;
 import jm.task.core.jdbc.model.User;
 import jm.task.core.jdbc.util.DatabaseUtil;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -19,11 +16,20 @@ public class UserDaoJDBCImpl implements UserDao {
     private static final String GET_ALL_USERS_QUERY = "SELECT * FROM users";
     private static final String CLEAN_USERS_TABLE_QUERY = "TRUNCATE TABLE users";
 
+    Connection connection;
+
+    {
+        try {
+            connection = DatabaseUtil.getConnection();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
     @Override
     public void createUsersTable() {
-        try (Connection connection = DatabaseUtil.getConnection();
-             PreparedStatement preparedStatement = connection.prepareStatement(CREATE_TABLE_QUERY)) {
-            preparedStatement.executeUpdate();
+        try (Statement statement = connection.createStatement()) {
+            statement.executeUpdate(CREATE_TABLE_QUERY);
         } catch (SQLException e) {
             e.printStackTrace();
         }
@@ -31,18 +37,17 @@ public class UserDaoJDBCImpl implements UserDao {
 
     @Override
     public void dropUsersTable() {
-        try (Connection connection = DatabaseUtil.getConnection();
-             PreparedStatement preparedStatement = connection.prepareStatement(DROP_TABLE_QUERY)) {
-            preparedStatement.executeUpdate();
+        try (Statement statement = connection.createStatement()) {
+            statement.executeUpdate(DROP_TABLE_QUERY);
         } catch (SQLException e) {
             e.printStackTrace();
         }
     }
 
+
     @Override
     public void saveUser(String name, String lastName, byte age) {
-        try (Connection connection = DatabaseUtil.getConnection();
-             PreparedStatement preparedStatement = connection.prepareStatement(SAVE_USER_QUERY)) {
+        try (PreparedStatement preparedStatement = connection.prepareStatement(SAVE_USER_QUERY)) {
             preparedStatement.setString(1, name);
             preparedStatement.setString(2, lastName);
             preparedStatement.setByte(3, age);
@@ -54,8 +59,7 @@ public class UserDaoJDBCImpl implements UserDao {
 
     @Override
     public void removeUserById(long id) {
-        try (Connection connection = DatabaseUtil.getConnection();
-             PreparedStatement preparedStatement = connection.prepareStatement(REMOVE_USER_BY_ID_QUERY)) {
+        try (PreparedStatement preparedStatement = connection.prepareStatement(REMOVE_USER_BY_ID_QUERY)) {
             preparedStatement.setLong(1, id);
             preparedStatement.executeUpdate();
         } catch (SQLException e) {
@@ -66,9 +70,8 @@ public class UserDaoJDBCImpl implements UserDao {
     @Override
     public List<User> getAllUsers() {
         List<User> users = new ArrayList<>();
-        try (Connection connection = DatabaseUtil.getConnection();
-             PreparedStatement preparedStatement = connection.prepareStatement(GET_ALL_USERS_QUERY);
-             ResultSet resultSet = preparedStatement.executeQuery()) {
+        try (Statement statement = connection.createStatement();
+             ResultSet resultSet = statement.executeQuery(GET_ALL_USERS_QUERY)) {
             while (resultSet.next()) {
                 User user = new User();
                 user.setId(resultSet.getLong("id"));
@@ -85,9 +88,8 @@ public class UserDaoJDBCImpl implements UserDao {
 
     @Override
     public void cleanUsersTable() {
-        try (Connection connection = DatabaseUtil.getConnection();
-             PreparedStatement preparedStatement = connection.prepareStatement(CLEAN_USERS_TABLE_QUERY)) {
-            preparedStatement.executeUpdate();
+        try (Statement statement = connection.createStatement()) {
+            statement.executeUpdate(CLEAN_USERS_TABLE_QUERY);
         } catch (SQLException e) {
             e.printStackTrace();
         }
